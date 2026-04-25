d18218c6d94816a65f3970fc7486b488d557b60c
leandro-afsuporte <leandropaiva.afsuporte@gmail.com>
2026-04-25 13:44:22 -0300

    Primeira versão com banco

--- api/app.py
+++ api/app.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
+from supabase import create_client
+import os
 
 app = Flask(
     __name__,
     template_folder="../templates",
     static_folder="../static"
 )
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 @app.route("/")
 def home():
