5d3662d6409a593ae6eeb2ef1988861d2e2cb04e
leandro-afsuporte <leandropaiva.afsuporte@gmail.com>
2026-04-25 14:37:29 -0300

    Primeira versão com banco2

--- api/app.py
+++ api/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
 from supabase import create_client
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 app = Flask(
     __name__,
@@ -11,12 +14,28 @@ app = Flask(
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
+if not SUPABASE_URL or not SUPABASE_KEY:
+    raise Exception("SUPABASE_URL ou SUPABASE_KEY não configuradas")
+
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 @app.route("/")
 def home():
     return render_template("tela_inicial.html")
 
-# Handler para Vercel
+@app.route("/teste-supabase")
+def teste_supabase():
+    try:
+        resposta = supabase.table("usuarios").select("*").limit(1).execute()
+        return {
+            "status": "conectado",
+            "dados": resposta.data
+        }
+    except Exception as e:
+        return {
+            "status": "erro",
+            "mensagem": str(e)
+        }
+
 def handler(request, *args, **kwargs):
     return app(request.environ, lambda *args: None)
